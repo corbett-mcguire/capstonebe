b9cc98fe0030c456277c49a11db32b7c79b6f202
Corbett McGuire <corbett.mcguire@gmail.com>
2022-02-01 15:10:39 -0500

    added and worked on post

---
+++ src/main/java/com/example/model/Post.java
@@ -0,0 +1,74 @@
+package com.example.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jdk.jfr.Category;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Posts")
+public class Post {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", title=" + title + '\'' +
+                '}';
+    }
+
+}
