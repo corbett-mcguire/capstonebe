13448212f8d638a9a9a1adb2790d277f064f3db3
Corbett McGuire <corbett.mcguire@gmail.com>
2022-02-02 15:26:35 -0500

    refactored mapping to type

--- src/main/java/com/example/controller/CapstoneController.java
+++ src/main/java/com/example/controller/CapstoneController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.model.Post;
+import com.example.model.Type;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -21,21 +23,33 @@ public class CapstoneController {
     public void setCapstoneRepository(CapstoneRepository capstoneRepository) {
         this.capstoneRepository = capstoneRepository;
     }
-
-    @GetMapping("/posts")
-    public List<Post> getPosts() {
-        System.out.println("calling getPosts ==>");
-        return capstoneRepository.getPosts();
+    // http://localhost:9092/api/types
+    @GetMapping("/types")
+    public List<Type> getTypes() {
+        System.out.println("calling getTypes method from controller");
+        return capstoneRepository.getTypes();
     }
 
-    @GetMapping(path = "/posts/{postId}")
-    public Optional getPost(@PathVariable Long postId) {
-        System.out.println("Calling getPost ==>");
-        Optional posts = CapstoneRepository.findById(postId);
-        if (posts.isPresent()) {
-            return posts;
-        } else {
-            throw new InformationNotFoundException("post with id " + categoryId + " not found");
-        }
+    // http://localhost:9092/api/types/1
+    @GetMapping(path = "/types/{typeId}")
+    public Type getType(@PathVariable Long typeId) {
+        System.out.println("Calling getType ==>");
+        return capstoneRepository.getType(typeId);
+    }
+
+    // http://localhost:9092/api/types
+    @GetMapping(path = "/types")
+    public Type createType(@RequestBody Type typeObject) {
+        System.out.println("Calling createType ==>");
+        return capstoneRepository.createType(typeObject);
+    }
+
+    // http://localhost:9092/api/types/1
+    @PutMapping(path = "/types/{typeId}")
+    public Type updateType(@PathVariable(value = "typeId") Long typeId, @RequestBody Type typeObject) {
+        System.out.println("calling updateType method from controller");
+        return capstoneRepository.updateType(typeId, typeObject);
+    }
+
 
 }
--- src/main/java/com/example/repository/CapstoneRepository.java
+++ src/main/java/com/example/repository/CapstoneRepository.java
@@ -1,11 +1,29 @@
 package com.example.repository;
 
 import com.example.model.Post;
+import com.example.model.Type;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CapstoneRepository extends Repository{
     List<Post> getPosts();
+
+    Optional getPosts(Long postId);
+
+    Post createPost(Post categoryObject);
+
+    Post getPost(Long postId);
+
+    Post updatePost(Long categoryId, Post categoryObject);
+
+    List<Type> getTypes();
+
+    Type getType(Long typeId);
+
+    Type updateType(Long typeId, Type typeObject);
+
+    Type createType(Type typeObject);
 }
